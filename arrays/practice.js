160497504010373d301208ca36712568eb38fa2e
Katie <adlerk8@gmail.com>
2020-10-08 14:47:55 -0700

    problems 1-5

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,8 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
-
+function first(arr) {
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +34,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[2];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +51,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(let i = 0; i <= family.length-1; i++)
+  alert(family[i]);
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for(let i= letters.length - 1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,9 +88,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
-
+function evenFinder(nums){
+  let evenNums = [];
+  for(let i = 0; i <= nums.length - 1; i++){
+    if (nums[i] % 2 === 0){
+      evenNums.push(nums[i])
+  }
+}
+  return evenNums;
+}
 
 
 
@@ -107,7 +123,6 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
 
 
 ////////// PROBLEM 7 //////////
