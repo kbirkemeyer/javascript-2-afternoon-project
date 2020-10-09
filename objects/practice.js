088c7a62ed6213df40d515d549f14f41afa61fe1
Katie <adlerk8@gmail.com>
2020-10-08 20:21:42 -0700

    ready for review

--- objects/practice.js
+++ objects/practice.js
@@ -15,7 +15,11 @@
 */
 
 //Code here
-
+let me = {
+  name: "Katie",
+  age: 30
+}
+//alert(me.name);
 ////////// PROBLEM 2 //////////
 
 /*
@@ -24,21 +28,30 @@
 */
 
 //Code here
-
+let favoriteThings = {
+  band: "Rainbow Kitten Surprise",
+  food: "beans and rice",
+  person: "Benhameen",
+  book: "Cloud Atlas",
+  movie: "Remember the Titans",
+  holiday: "Valentine's Day"
+}
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
-
+favoriteThings.car = "Subaru Baja";
+favoriteThings.brand = "Free People";
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 ////////// PROBLEM 3 //////////
 
 /*
@@ -50,13 +63,15 @@
 */
 
 //Code here
-
+let backPack = {};
+let item = 'firstPocket';
+backPack[item] = "chapstick";
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+//alert(backPack);
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,7 +79,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack);
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +100,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
